1758c6b059d6f001ca61b746a114e980e4e94cb7
cccat-best <1066801547@qq.com>
2022-09-21 22:24:43 +0800

    添加一些配置

---
+++ unpackage/dist/dev/mp-weixin/pages/old/home/home.js
@@ -0,0 +1,33 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/old/home/home"],{
+
+/***/ 66:
+/*!*******************************************************************************!*\
+  !*** D:/Desktop/2022-shiyi_hua5/main.js?{"page":"pages%2Fold%2Fhome%2Fhome"} ***!
+  \*******************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+
+
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 4));
+var _home = _interopRequireDefault(__webpack_require__(/*! ./pages/old/home/home.vue */ 67));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };} // @ts-ignore
+wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;createPage(_home.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 67:
+/*!**********************************************************!*\
+  !*** D:/Desktop/2022-shiyi_hua5/pages/old/home/home.vue ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'D:\\Desktop\\2022-shiyi_hua5\\pages\\old\\home\\home.vue'");
+
+/***/ })
+
+},[[66,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../../.sourcemap/mp-weixin/pages/old/home/home.js.map
