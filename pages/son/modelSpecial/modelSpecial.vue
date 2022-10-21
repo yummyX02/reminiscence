b59721c9c76a19abfebf45dcdd9551bf19e762c1
cccat-best <1066801547@qq.com>
2022-10-21 16:24:05 +0800

    定制语音包完成

--- pages/son/modelSpecial/modelSpecial.vue
+++ pages/son/modelSpecial/modelSpecial.vue
@@ -4,15 +4,20 @@
       <image id="imge" src="../../../static/shouyinji-2.png"></image>
     </view>
     <view class="remind">
-      <input id="ipt" type="text" placeholder="请输入提醒内容" />
+      <input
+        id="ipt"
+        type="text"
+        placeholder="请输入提醒内容"
+        v-model="title"
+      />
       <text id="txt">最多八个字</text>
     </view>
     <view class="bottom">
-      <div class="left">
+      <div class="left" @click="playVoice">
         <image id="icon" src="../../../static/bofang-2.png"></image>
         <div class="text">试听</div>
       </div>
-      <div class="middle" @longtap="saysay" @touchend="saydone">
+      <div class="middle">
         <image
           v-if="isShow"
           id="icon1"
@@ -25,8 +30,8 @@
           src="../../../static/tubiaozhizuo-.png"
           @click="gotoFinish()"
         ></image>
-        <div class="text" v-if="isShow">点击开始</div>
-        <div class="text" v-else @click="gotoFinish()">点击结束</div>
+        <div class="text" v-if="isShow" style="font-size: 1.3em">点击开始</div>
+        <div class="text" v-else style="font-size: 1.3em">点击结束</div>
       </div>
       <div class="right" @click="gotoSelect()">
         <image id="icon" src="../../../static/24gl-nextCircle.png"></image>
@@ -41,6 +46,15 @@ export default {
   data() {
     return {
       isShow: true,
+      title: "",
+      voicePath: "",
+      voiceLength: 0,
+      recorderManager: {},
+      innerAudioContext: {},
+      getLength: {
+        startTimestamp: 0,
+        endTimestamp: 0,
+      },
     };
   },
   onLoad() {
@@ -51,55 +65,18 @@ export default {
     let self = this;
     this.recorderManager.onStop(function (res) {
       console.log("recorder stop" + JSON.stringify(res));
+      self.getLength.endTimestamp = Date.now();
       self.voicePath = res.tempFilePath;
-      self.voiceLength = res.duration;
+      self.voiceLength = parseInt(
+        (self.getLength.endTimestamp - self.getLength.startTimestamp) / 1000 + 1
+      );
     });
   },
   methods: {
-    change() {
-      this.isShow = !this.isShow;
-      this.saysay();
-      this.startRecord();
-    },
-    gotoSelect() {
-      console.log("我去语音包界面啦~");
-      uni.redirectTo({
-        url: "/pages/son/selectPkg/selectPkg",
-      });
-      // 提交语音包接口
-      const data = {
-        userId: uni.getStorageSync("userId"),
-        data:""
-      };
-      uni.$http.post("/submit/child/voice-packet/submit",data)
-      .then((res)=>{
-        console.log(res);
-      })
-      .catch((err)=>{
-        console.log(err);
-      })
-    },
-    gotoFinish() {
-      this.saydone();
-      this.endRecord();
-      uni.showToast({
-        title: "录制结束",
-        icon: "success",
-        duration: 2000,
-      });
-      this.isShow = !this.isShow;
-    },
     // 录音部分
-    saysay() {
-      console.log("saysay");
-      this.startRecord();
-    },
-    saydone() {
-      console.log("saydone");
-      this.endRecord();
-    },
     startRecord() {
       console.log("开始录音");
+      this.getLength.startTimestamp = Date.now();
       this.recorderManager.start({
         sampleRate: 16000, //采样率，App、小程序
         //encodeBitRate:96000,//仅小程序支持编码码率
@@ -119,6 +96,58 @@ export default {
         this.innerAudioContext.src = this.voicePath;
         this.innerAudioContext.play();
       }
+    },
+    saysay() {
+      console.log("saysay");
+      this.startRecord();
+    },
+    saydone() {
+      console.log("saydone");
+      this.endRecord();
+    },
+    change() {
+      this.isShow = !this.isShow;
+      this.saysay();
+    },
+    gotoFinish() {
+      this.saydone();
+      uni.showToast({
+        title: "录制结束",
+        icon: "success",
+        duration: 2000,
+      });
+      this.isShow = !this.isShow;
+    },
+    gotoSelect() {
+      let formData = {
+        userId: uni.getStorageSync("userId"),
+        data: this.title,
+        length: this.voiceLength,
+      };
+      console.log(formData);
+      // 提交语音包接口
+      uni.uploadFile({
+        url: "https://43.142.146.75:38080/child/voice-packet/submit", //仅为示例，非真实的接口地址
+        filePath: this.voicePath,
+        name: "file",
+        formData: formData,
+        header: {
+          "content-type": "multipart/form-data",
+          Authorization: uni.getStorageSync("token"),
+        },
+        success: (uploadFileRes) => {
+          console.log("upload", JSON.parse(uploadFileRes.data));
+          let data = JSON.parse(uploadFileRes.data);
+          if (data.code == "00000") {
+            uni.showToast({ title: "定制语音包成功" });
+            setTimeout(() => {
+              uni.reLaunch({
+                url: "/pages/son/selectPkg/selectPkg",
+              });
+            });
+          }
+        },
+      });
     },
   },
 };
