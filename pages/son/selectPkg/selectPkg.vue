881d4940b84859e0b24088a95c79db62680afb02
yummyX02 <1245191297@qq.com>
2022-10-22 22:29:42 +0800

    2022.10.22初步完成

--- pages/son/selectPkg/selectPkg.vue
+++ pages/son/selectPkg/selectPkg.vue
@@ -75,7 +75,10 @@ export default {
         .get("/child/outbox/all-video-packet", data)
         .then((res) => {
           console.log("获取语音包列表啦~", res);
-          this.voiceList = res.data.data;
+          if (res.data.code === "00000") {
+            console.log("获取所有语音包成功");
+            this.voiceList = res.data.data;
+          }
         })
         .catch((err) => {
           console.log(err);
